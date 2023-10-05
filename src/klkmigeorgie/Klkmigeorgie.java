3912c824d99fd8e2371dc8bb3750ecc606161364
Arturo <Arturo@192.168.56.1>
2023-10-05 02:35:34 -0500

    Cambios de prueba

--- src/klkmigeorgie/Klkmigeorgie.java
+++ src/klkmigeorgie/Klkmigeorgie.java
@@ -10,9 +10,6 @@ package klkmigeorgie;
  */
 public class Klkmigeorgie {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
